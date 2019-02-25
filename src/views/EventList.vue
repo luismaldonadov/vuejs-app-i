2eb9b6c0640aace00d0a7d08d80e8408ce0509b4
Luis Maldonado <maldonadovla@gmail.com>
2019-02-25 09:17:30 -0700

    Global Event service for api calls

---
+++ src/views/EventList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Event Listing</h1>
+    <EventCard
+      v-for="event in events"
+      :key="event.id"
+      :event="event"
+    />
+    <BaseIcon />
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue';
+import EventService from '@/services/EventService';
+
+export default {
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [],
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => {
+        this.events = response.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
